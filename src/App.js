7b82b715ac503cfdd546b5b61d0e3e21888c7f21
devmateusramos <devmateusramos@gmail.com>
2022-02-10 09:09:55 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const App = () => <h1>App</h1>;
+import MainPage from './pages/main/Main'
+import Login from './pages/login/Login'
 
-export default App;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" component={MainPage} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
